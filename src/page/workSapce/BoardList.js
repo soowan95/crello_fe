8f09d0cf470134aecd737fede5699982413b22b8
soowan95 <ksw9565@gmail.com>
2024-01-05 10:44:42 +0900

    feat/보드 추가 기능 완료

--- src/page/workSapce/BoardList.js
+++ src/page/workSapce/BoardList.js
@@ -1,46 +1,62 @@
 import { Box, Flex, Heading } from "@chakra-ui/react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faClock } from "@fortawesome/free-regular-svg-icons";
+import axios from "axios";
 
-function BoardList({ boards }) {
+function BoardList({ boards, recentBoard }) {
+  const handleBoard = (id) => {
+    axios
+      .put(
+        "/api/v1/board/updateRecent",
+        {
+          id,
+        },
+        {
+          headers: {
+            Authorization: "Bearer " + localStorage.getItem("accessToken"),
+          },
+        },
+      )
+      .then(() => console.log("ok"));
+  };
+
   return (
     <Box>
-      <Box w={"90%"} h={"150px"} m={"50px auto"}>
-        <Box h={"30px"}>
+      <Box w={"90%"} m={"80px auto"}>
+        <Box h={"50px"} fontSize={"1.5rem"}>
           <FontAwesomeIcon icon={faClock} /> Recently worked
         </Box>
-        <Flex w={"100%"} h={"120px"}>
-          {boards.length !== 0 &&
-            boards.map((board, idx) => (
-              <Box
-                pl={3}
-                key={idx}
-                w={"20%"}
-                h={"120px"}
-                bg={"#1d285d"}
-                borderRadius={"10px"}
-                lineHeight={"40px"}
-              >
-                board
-              </Box>
-            ))}
-        </Flex>
+        <Box
+          mt={"20px"}
+          pl={3}
+          w={"20%"}
+          h={"200px"}
+          bg={"#1d285d"}
+          borderRadius={"10px"}
+          lineHeight={"40px"}
+          fontSize={"1.2rem"}
+          onClick={() => handleBoard(recentBoard.id)}
+        >
+          {recentBoard !== null && recentBoard.title}
+        </Box>
       </Box>
-      <Box w={"90%"} h={"200px"} m={"80px auto"}>
+      <Box w={"90%"} m={"80px auto"}>
         <Heading h={"80px"}>Your Workspaces</Heading>
-        <Flex w={"100%"} h={"120px"} gap={"6%"}>
+        <Flex w={"100%"} h={"200px"} gap={"6%"}>
           {boards.length !== 0 &&
             boards.map((board, idx) => (
               <Box
                 pl={3}
                 key={idx}
                 w={"20%"}
-                h={"120px"}
+                h={"100%"}
                 bg={"#1d285d"}
                 borderRadius={"10px"}
                 lineHeight={"40px"}
+                fontSize={"1.2rem"}
+                onClick={() => handleBoard(board.id)}
               >
-                board
+                {board !== null && board.title}
               </Box>
             ))}
         </Flex>
